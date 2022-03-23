05d8dcb6b33c0b7bd2cac8694833586ba6eadf6c
jimbo23 <kiefersoon@gmail.com>
2022-03-23 12:07:56 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 // only require this when it's run in JS.
 // TS will be transpiled to JS during runtime
+
 if (!process.env._IS_TS_NODE) {
   require('module-alias/register');
 }
 
+import { AppModule } from '@app/app.module';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
