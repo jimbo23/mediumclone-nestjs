096542ee12175122c514c9d2f69a9e55cec4fd49
jimbo23 <kiefersoon@gmail.com>
2022-03-27 10:31:28 +0800

    creaet article module && controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,18 @@ import { TagModule } from '@app/tag/tag.module';
 import { UserModule } from '@app/user/user.module';
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticleModule } from './article/article.module';
 import ormconfig from './ormconfig';
 
 @Module({
   // Our application is configured in a module with a typeorm
   // with the ormconfig.
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    TagModule,
+    UserModule,
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
