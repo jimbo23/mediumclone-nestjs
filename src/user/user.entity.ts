113aac6e3d2139f0537963f0ac86cbfd16d0e752
jimbo23 <kiefersoon@gmail.com>
2022-03-27 19:48:02 +0800

    build relationship between user and article

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,5 +1,12 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { hash } from 'bcrypt';
+import { ArticleEntity } from '@app/article/article.entity';
 
 @Entity({ name: 'users' })
 export class UserEntity {
@@ -29,4 +36,7 @@ export class UserEntity {
     // res = await hash(plainPassword, salt)
     this.password = await hash(this.password, 10);
   }
+
+  @OneToMany(() => ArticleEntity, (article) => article.author)
+  articles: ArticleEntity[];
 }
