7505344ce7cfcb510c94c2c2c24992db87d007c7
jimbo23 <kiefersoon@gmail.com>
2022-03-28 15:04:16 +0800

    update http script for manual testing

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -5,6 +5,7 @@ import { UserEntity } from '@app/user/user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import slugify from 'slugify';
 
 @Injectable()
 export class ArticleService {
@@ -25,9 +26,14 @@ export class ArticleService {
 
     article.author = currentUser;
 
-    article.slug = 'foo';
+    article.slug = this.getSlug(createArticleDto.title);
 
     return await this.articleRepository.save(article);
+  }
+
+  private getSlug(title: string): string {
+    const uniqueString = ((Math.random() * Math.pow(36, 6)) | 0).toString(36);
+    return slugify(title, { lower: true }) + '-' + uniqueString;
   }
 
   buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
