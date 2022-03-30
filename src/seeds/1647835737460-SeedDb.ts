a626f670d4d0f85d000c524ed9e11bba55b03d86
jimbo23 <kiefersoon@gmail.com>
2022-03-30 20:54:13 +0800

    write sql for seeding db

--- src/seeds/1647835737460-SeedDb.ts
+++ src/seeds/1647835737460-SeedDb.ts
@@ -7,6 +7,15 @@ export class SeedDb1647835737460 implements MigrationInterface {
     await queryRunner.query(
       `INSERT INTO tags (name) VALUES ('dragons'), ('coffee'), ('nestjs')`,
     );
+
+    await queryRunner.query(
+      // password is kiefer
+      `INSERT INTO users (username, email, password) VALUES ('kiefer','kiefer@gmail.com', '$2b$10$0IK5Gujv21Iow6B8Qp2DhOrEkV2Nz9KMKV/Q8UDyGaNHmDOyVPLrO')`,
+    );
+
+    await queryRunner.query(
+      `INSERT INTO articles (slug, title, description, body, "tagList", "authorId") VALUES ('first-article', 'First Article', 'First Article Description', 'First Article Body', 'coffee,dragons', 1),('second-article', 'Second Article', 'Second Article Description', 'Second Article Body', 'coffee,dragons', 1)`,
+    );
   }
 
   public async down(): Promise<void> {}
