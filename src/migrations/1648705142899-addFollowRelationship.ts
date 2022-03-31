f8dc848400662321f9c01ef024a4ed03f30c0a42
jimbo23 <kiefersoon@gmail.com>
2022-03-31 14:45:06 +0800

    update follows table name

--- src/migrations/1648705142899-addFollowRelationship.ts
+++ src/migrations/1648705142899-addFollowRelationship.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class addFollowRelationship1648705142899 implements MigrationInterface {
-    name = 'addFollowRelationship1648705142899'
+  name = 'addFollowRelationship1648705142899';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "follow_entity" ("id" SERIAL NOT NULL, "followerId" integer NOT NULL, "followingId" integer NOT NULL, CONSTRAINT "PK_18966373213f8c51750c227943b" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "follows" ("id" SERIAL NOT NULL, "followerId" integer NOT NULL, "followingId" integer NOT NULL, CONSTRAINT "PK_18966373213f8c51750c227943b" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "follow_entity"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "follows"`);
+  }
 }
--- src/profile/follow.entity.ts
+++ src/profile/follow.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity('follows')
 export class FollowEntity {
   @PrimaryGeneratedColumn()
   id: number;
