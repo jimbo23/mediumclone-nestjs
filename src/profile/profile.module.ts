c921793b74ba19a182534239f7c07ed32d8e08e7
jimbo23 <kiefersoon@gmail.com>
2022-03-31 13:53:54 +0800

    update imports in profile.module.ts! important

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -3,9 +3,10 @@ import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '@app/user/user.entity';
+import { FollowEntity } from '@app/profile/follow.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, FollowEntity])],
   controllers: [ProfileController],
   providers: [ProfileService],
 })
